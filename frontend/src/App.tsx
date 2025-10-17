78b2cd1e7d286fb7ac14c36ba799c06502223653
VitorHugodeOliv <vishigo.vh@gmail.com>
2025-10-17 14:02:52 -0300

    criando rota da homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router";
 import RotaLogin from "./routes/RotaLogin";
 import RotaRegistro from "./routes/RotaRegistro";
 import RotaDashboard from "./routes/RotaDashboard";
+import RotaHomePage from "./routes/RotaHomePage";
 
 const App: React.FC = () => {
 
   return (
     <Routes>
+      <Route path="/" element={<RotaHomePage/>}/>
       <Route path="/login" element={<RotaLogin/>}/>
       <Route path="/registro" element={<RotaRegistro/>}/>
       <Route path="/dashboard" element={ <RotaDashboard/>}/>
---
+++ frontend/src/routes/RotaHomePage.tsx
@@ -0,0 +1,9 @@
+function RotaHomePage () {
+    return (
+        <div>
+            aqui é a homepage
+        </div>
+    )
+};
+
+export default RotaHomePage;
