bc7a11db54207d9d067be39fe5d35c52b3d51e7a
VitorHugodeOliv <vishigo.vh@gmail.com>
2025-10-22 14:24:13 -0300

    configurando ambiente para testes no front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { configDefaults, defineConfig as defineVitestConfig } from "vitest/config";
 
-// https://vite.dev/config/
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineVitestConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    exclude: [...configDefaults.exclude, "e2e/*"],
+  },
+});
